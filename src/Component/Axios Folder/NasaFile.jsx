91f7f6891aaace6060a9382bfafe90ba14702279
Ale <alearanibar@gmail.com>
2019-10-10 22:36:15 -0700

    done

--- src/Component/Axios Folder/NasaFile.jsx
+++ src/Component/Axios Folder/NasaFile.jsx
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import NasaCard from './NasaCard'
 
-
 const NasaFile = () => {
-    const [nasa, setNasa] = useState([])
-    // let nasaKey = `JGYKNM6h2ZRuzKYCFlKHU3q8PovytMseIoTGK5h0`
-    // let pullData = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`
-       let pullData = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-10-09`
+    let separate = new Date ();
+    console.log(separate)
+    console.log({separate})
+    let month = separate.getUTCMonth( +1);
+    let day = separate.getUTCDate();
+    let year = separate.getUTCFullYear();
 
+    let date = `${year}-${month}-${day}`
+
+    const [nasa, setNasa] = useState([])
+    const [newDate, setNewDate] = useState(date)
+
+    let nasaKey = `JGYKNM6h2ZRuzKYCFlKHU3q8PovytMseIoTGK5h0&date=${newDate}`
+    let pullData = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`
+ 
     useEffect(() => {
         axios.get(pullData)
             .then(response => {
@@ -18,11 +27,10 @@ const NasaFile = () => {
             .catch(error =>{
                 console.log(`No Good`, error)
             })
-    }, [])
+    }, [newDate])
     return(
         <div className='info-card'>
-          
-                <NasaCard key={nasa.index} title={nasa.title} date={nasa.date} explanation={nasa.explanation} hdurl={nasa.hdurl}   />
+                <NasaCard key={nasa.index} title={nasa.title} date={nasa.date} explanation={nasa.explanation} hdurl={nasa.hdurl} neDate={newDate} setNewDate={setNewDate}  />  
                 
         </div>
     )
